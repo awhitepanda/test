94bc3bacc604a3deae3bf2ebe047ef2e7b5d16b5
awhitepanda <nameqianxi@outlook.com>
2020-08-17 23:05:57 +0800

    2020-08-17 23:05:51 by Python脚本推送提交

---
+++ GitCmd.py
@@ -0,0 +1,56 @@
+import os
+import time
+
+
+
+class GitCmd(object):
+    """Git推送提交的构造函数
+    获取当前工作路径
+    rootpath 更改工作路径到指定根目录
+    filepath 更改工作路径到指定路径
+    git指令：追踪所有文件
+    git指令：提交更改
+    git指令：推送到远程仓库
+    保存日志
+    """
+
+    def __init__(self, filepath, rootpath="D:"):
+        print("current work directory" + os.getcwd())
+        os.chdir(rootpath)
+        os.chdir(filepath)
+        print("into work repository" + os.getcwd())
+
+    def git(self):
+        """
+        git pull
+        git add
+        git commit
+        git push
+        :return:
+        """
+        pull = "git pull"
+        add = "git add ."
+        commit = '''git commit -a -m "''' \
+                 + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) \
+                 + ''' by Python脚本推送提交"'''
+        push = "git push"
+
+        self.cmd(pull)
+        self.cmd(add)
+        self.cmd(commit)
+        self.cmd(push)
+
+    def cmd(self, command):
+        """
+        :param command:
+        :return:
+        """
+        # os.system(commond)
+        log = os.popen(command).readlines()
+
+
+if __name__ == "__main__":
+    GitCmd(os.getcwd()).git()
+    print("what")
+    os.system("pause")
+    pass
